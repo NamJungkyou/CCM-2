304c25c0e99799d99cb2ce9212d78751ef03076f
jaehwanspin <jaehwanspin@gmail.com>
2018-07-30 15:49:21 +0900

    DTO 인터페이스 주석단거

--- Eureca/src/ccm/data/DTO.java
+++ Eureca/src/ccm/data/DTO.java
@@ -3,7 +3,23 @@ package ccm.data;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public interface DTO
-{
-	public void setParams(ResultSet rs) throws SQLException;
+/**
+ * 
+ * SQL 쿼리문 실행 후 객체에 값을 저장할 때
+ * DAO에서의 가독성을 높임과 동시에
+ * 클래스 객체 데이터 초기화를 편하게 해주는 인터페이스
+ * 
+ * @author 글로벌 IT 경영 진재환
+ *
+ */
+
+public abstract interface DTO {
+	/**
+	 * DAO에서 쿼리 실행 후 ResultSet 객체로 받는
+	 * 변수들을 이 클래스 객체의 변수들에 세팅하는 메소드
+	 * 
+	 * @param paramResultSet
+	 * @throws SQLException
+	 */
+	public abstract void setParams(ResultSet paramResultSet) throws SQLException;
 }
