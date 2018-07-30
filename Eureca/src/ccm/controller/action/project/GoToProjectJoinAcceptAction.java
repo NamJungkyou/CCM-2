e229a39906a4be909ebd963610f3088a835a9cec
jaehwanspin <jaehwanspin@gmail.com>
2018-07-30 15:52:05 +0900

    액션들 주석단거

--- Eureca/src/ccm/controller/action/project/GoToProjectJoinAcceptAction.java
+++ Eureca/src/ccm/controller/action/project/GoToProjectJoinAcceptAction.java
@@ -1,50 +1,88 @@
 package ccm.controller.action.project;
 
+import ccm.controller.action.Action;
+import ccm.dao.ProjectAdministrationDAO;
+import ccm.data.ParamInt;
+import ccm.data.function.ProjUtil;
 import java.io.IOException;
-
+import java.io.PrintStream;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import ccm.controller.action.Action;
-import ccm.dao.ProjectAdministrationDAO;
-import ccm.data.ParamInt;
-import ccm.data.ProjJoinedFreelancerForList;
-import ccm.data.function.ProjUtil;
+/**
+ * 프로젝트 참여신청 접수 페이지로 이동하는 액션
+ * 
+ * @author 글로벌 IT 경영 진재환
+ *
+ */
 
 public class GoToProjectJoinAcceptAction implements Action {
-	@Override
+	public GoToProjectJoinAcceptAction() {
+	}
+
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 한글 설정
 		request.setCharacterEncoding("UTF-8");
+		
+		// 프리랜서 이름 검색옵션을 받아옴
 		String searchFreelancerName = request.getParameter("searchFreelancerName");
+		
 		if (searchFreelancerName != null)
+			// UTF-8 인코딩으로 다시 설정해줌
 			searchFreelancerName = new String(searchFreelancerName.getBytes("8859_1"), "UTF-8");
-		System.out.println("야 간지난다!!!!!!! " + searchFreelancerName);
+		
 		searchFreelancerName = searchFreelancerName == null ? "" : searchFreelancerName;
-		String pageNumParam = request.getParameter("projJoinedFreePageNum"); // 페이지번호
-		Integer pageNum = pageNumParam != null ? Integer.parseInt(pageNumParam) : 1;
+		
+		// 현재 페이지 번호
+		String pageNumParam = request.getParameter("projJoinedFreePageNum");
+		
+		// 페이지 번호가 null 이면 기본값 1로 맞춰줌
+		Integer pageNum = Integer.valueOf(pageNumParam != null ? Integer.parseInt(pageNumParam) : 1);
+		
+		// 참여신청자 목록 정렬옵션이 null이어도 기본값 1로 맞춰줌
 		String listSortOptionParam = request.getParameter("listSortOption") == null ? "1"
 				: request.getParameter("listSortOption");
+		
+		
 		int listSortOption = Integer.parseInt(listSortOptionParam);
+		
+		// 정렬옵션을 코드에서 쿼리구문으로 변경해줌
 		String listSortOptionString = ProjUtil.getInstance().projJoinAcceptListCodeToOption(listSortOption);
+		
+		// 페이지번호를 저장하는 객체변수
 		ParamInt numOfList = new ParamInt(0);
+		
+		// DAO 인스턴스를 받아옴
 		ProjectAdministrationDAO pDao = ProjectAdministrationDAO.getInstance();
 
-		ProjJoinedFreelancerForList[] list = null;
-		list = pDao.getJoinedFreelancerList(listSortOptionString, numOfList, pageNum, new String(searchFreelancerName));
+		// 투입대상자 목록을 null로 초기화하고
+		ccm.data.ProjJoinedFreelancerForList[] list = null;
+		
+		// DB에서 목록을 받아옴
+		list = pDao.getJoinedFreelancerList(listSortOptionString, numOfList, pageNum.intValue(),
+				new String(searchFreelancerName));
 
-		int intNumOfPage = (int) Math.ceil(((double) numOfList.getIntValue() / 10.0f));
+		// 현재 모든 페이지의 개수를 계산함
+		int intNumOfPage = (int) Math.ceil(numOfList.getIntValue() / 10.0D);
 
-		int firstPage = (int) (Math.round((pageNum / 10.0f)) * 10) + 1;
-		int lastPage = (int) Math.min(firstPage + 9, intNumOfPage);
-		System.out.println("페이지넘 !!!!!!!!!!!!!!!!!!!! " + pageNum);
+		// 첫페이지
+		int firstPage = Math.round(pageNum.intValue() / 10.0F) * 10 + 1;
+		
+		// 마지막페이지
+		int lastPage = Math.min(firstPage + 9, intNumOfPage);
+		
+		// 요청값에 모든 변수를 넘겨줌
 		request.setAttribute("searchFreelancerName", searchFreelancerName);
-		request.setAttribute("numOfPage", intNumOfPage);
+		request.setAttribute("numOfPage", Integer.valueOf(intNumOfPage));
 		request.setAttribute("projJoinedFreePageNum", pageNum);
-		request.setAttribute("projJoinedFreeFirstPage", firstPage);
-		request.setAttribute("projJoinedFreeLastPage", lastPage);
+		request.setAttribute("projJoinedFreeFirstPage", Integer.valueOf(firstPage));
+		request.setAttribute("projJoinedFreeLastPage", Integer.valueOf(lastPage));
 		request.setAttribute("projJoinedFreeList", list);
-		request.setAttribute("listSortOption", listSortOption);
+		request.setAttribute("listSortOption", Integer.valueOf(listSortOption));
+		
+		// 페이지로 이동
 		request.getRequestDispatcher("/project/projectjoinaccept.jsp").forward(request, response);
 	}
 }
--- Eureca/src/ccm/controller/action/project/GoToPutFreelancerAction.java
+++ Eureca/src/ccm/controller/action/project/GoToPutFreelancerAction.java
@@ -1,13 +1,5 @@
 package ccm.controller.action.project;
 
-import java.io.IOException;
-import java.util.ArrayList;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import ccm.controller.action.Action;
 import ccm.dao.ProjectAdministrationDAO;
 import ccm.data.AllOfEuclidProject;
@@ -17,121 +9,158 @@ import ccm.data.ParamInt;
 import ccm.data.ProjectRecruitState;
 import ccm.data.RecommendedFreelancer;
 import ccm.data.function.ProjUtil;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-public class GoToPutFreelancerAction implements Action
-{
-	@SuppressWarnings("unchecked")
-	@Override
-	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-	{
-		String curRecommPageNumStr = request.getParameter("curRecommPageNum");
-		Integer curRecommPageNum = curRecommPageNumStr == null ? 1 : Integer.parseInt(curRecommPageNumStr);
-		String curAppliedFreePageNumStr = request.getParameter("curAppliedFreePageNum");
-		Integer curAppliedFreePageNum = curAppliedFreePageNumStr == null ?
-				1 : Integer.parseInt(curAppliedFreePageNumStr);
+/**
+ * 프리랜서 투입 페이지로 이동하는 액션
+ * 
+ * @author 글로벌 IT 경영 진재환
+ *
+ */
+public class GoToPutFreelancerAction implements Action {
+	public GoToPutFreelancerAction() {
+	}
+
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		// 현재 추천프리랜서 목록 페이지번호
+		// null이면 1로 맞춰줌
+		String curRecommPageNumStr = request.getParameter("curRecommPageNum");
+		Integer curRecommPageNum = Integer
+				.valueOf(curRecommPageNumStr == null ? 1 : Integer.parseInt(curRecommPageNumStr));
+		
+		// 현재 참여신청한 프리랜서 페이지번호
+		// null이면 1로 맞춰줌
+		String curAppliedFreePageNumStr = request.getParameter("curAppliedFreePageNum");
+		Integer curAppliedFreePageNum = Integer
+				.valueOf(curAppliedFreePageNumStr == null ? 1 : Integer.parseInt(curAppliedFreePageNumStr));
+
+		// 첫번째 추천프리랜서 정렬옵션을 받아옴
 		String recommlistSortOption1st = request.getParameter("recommListSortOption1st");
-			recommlistSortOption1st = recommlistSortOption1st != null ? recommlistSortOption1st : "5";
+		recommlistSortOption1st = recommlistSortOption1st != null ? recommlistSortOption1st : "5";
+		// 두번째 추천프리랜서 정렬옵션을 받아옴
 		String recommlistSortOption2st = request.getParameter("recommListSortOption2st");
-			recommlistSortOption2st = recommlistSortOption2st != null ? recommlistSortOption2st : "3";
+		recommlistSortOption2st = recommlistSortOption2st != null ? recommlistSortOption2st : "3";
+		// 세번째 추천프리랜서 정렬옵션을 받아옴
 		String recommlistSortOption3st = request.getParameter("recommListSortOption3st");
-			recommlistSortOption3st = recommlistSortOption3st != null ? recommlistSortOption3st : "2";
-			
-		System.out.println(recommlistSortOption1st + ", " + recommlistSortOption2st + ", " + recommlistSortOption3st
-				+ " ???????????$$$$$$$$$############@@@@@@@@@@@@@@@@@@@@ ");
-			
-		String recommSortOption[] =
-			{
+		recommlistSortOption3st = recommlistSortOption3st != null ? recommlistSortOption3st : "2";
+		
+		// 정렬옵션들을 배열 객체로 만들어줌
+		String[] recommSortOption = {
 				ProjUtil.getInstance().codeToRecommSortOption(Integer.parseInt(recommlistSortOption1st)),
 				ProjUtil.getInstance().codeToRecommSortOption(Integer.parseInt(recommlistSortOption2st)),
-				ProjUtil.getInstance().codeToRecommSortOption(Integer.parseInt(recommlistSortOption3st))
-			};
-		
+				ProjUtil.getInstance().codeToRecommSortOption(Integer.parseInt(recommlistSortOption3st)) };
+
+		// 참여신청한 프리랜서 목록의 정렬옵션
 		String appliedFreeListSortOption = request.getParameter("appliedFreeListSortOption");
-		System.out.println("쏠트옵션 !!!!!!!!!!!!!!!!!!!!!!!!!!! " + appliedFreeListSortOption);
+		
+		// null 이면 기본 1로 맞춰줌
 		appliedFreeListSortOption = appliedFreeListSortOption != null ? appliedFreeListSortOption : "1";
-		appliedFreeListSortOption =
-				ProjUtil.getInstance().codeToAppliedSortOption(Integer.parseInt(appliedFreeListSortOption));
-		System.out.println(appliedFreeListSortOption + " 옵션!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		
+		// 참여신청한 프리랜서 정렬옵션 코드를 SQL구문으로 바꿔줌
+		appliedFreeListSortOption = ProjUtil.getInstance()
+				.codeToAppliedSortOption(Integer.parseInt(appliedFreeListSortOption));
+		
+		// 투입대상자 목록의 정렬옵션을 받아옴
 		String freelancerListToPutInSortOption = request.getParameter("freelancerListToPutInSortOption");
-		
+
+		// 현재 선택된 프로젝트번호
 		String curProjNumStr = request.getParameter("curProjNum");
-		Integer curProjNum = curProjNumStr == null ?
-				ProjectAdministrationDAO.getInstance().getMaxProjNum() : Integer.parseInt(curProjNumStr);
-		
+		Integer curProjNum = Integer
+				.valueOf(curProjNumStr == null ? ProjectAdministrationDAO.getInstance().getMaxProjNum()
+						: Integer.parseInt(curProjNumStr));
+
+		// 세션에서 투입대상자 객체를 받아옴
 		HttpSession session = request.getSession();
-		ArrayList<FreeIdToPutInAndProjNum> freeIdListToPutIn = 
-				(ArrayList<FreeIdToPutInAndProjNum>) session.getAttribute("freelancerIdListToPutIn");
-		ArrayList<String> freeIdToPutIn = new ArrayList<String>();
+		ArrayList<FreeIdToPutInAndProjNum> freeIdListToPutIn = (ArrayList) session
+				.getAttribute("freelancerIdListToPutIn");
 		
-		if(freeIdListToPutIn != null)
-		{
-			for(int i = 0; i < freeIdListToPutIn.size(); i++)
-			{
-				if(freeIdListToPutIn.get(i).getProjNum() == curProjNum)
-				{
-					System.out.println(freeIdListToPutIn.get(i).getProjNum());
-					freeIdToPutIn.add(freeIdListToPutIn.get(i).getFreeId());
+		// 프리랜서 아이디만 투입대상자 목록에서 뽑아와 저장하는 리스트
+		ArrayList<String> freeIdToPutIn = new ArrayList();
+
+		if (freeIdListToPutIn != null) {
+			for (int i = 0; i < freeIdListToPutIn.size(); i++) {
+				if (((FreeIdToPutInAndProjNum) freeIdListToPutIn.get(i)).getProjNum() == curProjNum.intValue()) {
+					// 투입대상자 리스트에서 프리랜서 ID만 뽑아서 프리랜서 아이디 리스트에 저장함 
+					freeIdToPutIn.add(((FreeIdToPutInAndProjNum) freeIdListToPutIn.get(i)).getFreeId());
 				}
 			}
 		}
+
+		// DAO 인스턴스를 가져옴
+		ProjectAdministrationDAO p = ProjectAdministrationDAO.getInstance();
 		
-		ProjectAdministrationDAO p = ProjectAdministrationDAO.getInstance(); 
-		AllOfEuclidProject curProject = p.getProjectDetail(curProjNum);
-		ArrayList<ProjectRecruitState> projRecruitStateInfo = p.getRecruitState(curProjNum);
+		// 프로젝트 세부정보를 받아옴
+		AllOfEuclidProject curProject = p.getProjectDetail(curProjNum.intValue());
+		
+		// 현재 채용현황을 받아옴
+		ArrayList<ProjectRecruitState> projRecruitStateInfo = p.getRecruitState(curProjNum.intValue());
 		ParamInt recommNumOfRow = new ParamInt(0);
+		
+		// 추천프리랜서 목록을 받아옴
 		RecommendedFreelancer[] recommFreelancer = p.getRecommendedFreelancerList(recommSortOption,
-				curRecommPageNum, recommNumOfRow, curProjNum);
+				curRecommPageNum.intValue(), recommNumOfRow, curProjNum.intValue());
 		ParamInt appliedNumOfRow = new ParamInt(0);
-		JoinAppliedFreelancer[] appliedFreelancer = p.getAppliedFreelancer(curProjNum,
-				curAppliedFreePageNum, appliedFreeListSortOption, appliedNumOfRow);
-		ArrayList<JoinAppliedFreelancer> freelancerListToPutIn =
-				freeIdToPutIn == null || freeIdToPutIn.size() == 0 ? null :
-				p.getFreeListToPutIn(freeIdToPutIn, freelancerListToPutInSortOption);
 		
-		System.out.println("쓰쓰아웃 " + recommNumOfRow.getIntValue());
-		int recommFreeNumOfPage = (int)(Math.ceil((double)(recommNumOfRow.getIntValue()) / 10));
-		int recommFreeFirstPage = (int)((curRecommPageNum / 10) * 10) + 1;
-		int recommFreeLastPage = (int)Math.min(recommFreeFirstPage + 9, recommFreeNumOfPage);
-		System.out.println("넘오브페이지 : " + recommFreeNumOfPage
-				+ ", 퍼스트페이지 : " + recommFreeFirstPage + ", 라스트페이지 : " + recommFreeLastPage);
-		int appliedFreeNumOfPage = (int)(Math.ceil((double)(appliedNumOfRow.getIntValue()) / 10));
-		int appliedFreeFirstPage = (int)((curAppliedFreePageNum / 10) * 10) + 1;
-		int appliedFreeLastPage = (int)Math.min(appliedFreeFirstPage + 9, appliedFreeNumOfPage);
-		System.out.println(appliedFreeNumOfPage + "넘오브페이지");
+		// 참여신청한 프리랜서 목록
+		JoinAppliedFreelancer[] appliedFreelancer = p.getAppliedFreelancer(curProjNum.intValue(),
+				curAppliedFreePageNum.intValue(), appliedFreeListSortOption, appliedNumOfRow);
 		
-		System.out.println(ProjUtil.getInstance().recommSortOptionToCode(recommSortOption[0]) + "♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡");
-		System.out.println(ProjUtil.getInstance().recommSortOptionToCode(recommSortOption[1]) + "♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡");
-		System.out.println(ProjUtil.getInstance().recommSortOptionToCode(recommSortOption[2]) + "♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡");
+		// 투입대상자 목록 객체가 널이 아니거나 사이즈가 0이 아니면 프리랜서 투입 대상자 목록을 DB에서 가져옴 
+		ArrayList<JoinAppliedFreelancer> freelancerListToPutIn = (freeIdToPutIn == null) || (freeIdToPutIn.size() == 0)
+				? null
+				: p.getFreeListToPutIn(freeIdToPutIn, freelancerListToPutInSortOption);
+
+		// 추천프리랜서 전체 페이지 개수
+		int recommFreeNumOfPage = (int) Math.ceil(recommNumOfRow.getIntValue() / 10.0D);
+		
+		// 추천프리랜서 첫페이지 번호
+		int recommFreeFirstPage = curRecommPageNum.intValue() / 10 * 10 + 1;
+		
+		// 추천프리랜서 마지막페이지 번호
+		int recommFreeLastPage = Math.min(recommFreeFirstPage + 9, recommFreeNumOfPage);
+		
+		// 참여신청한 프리랜서도 똑같이 계산해줌
+		int appliedFreeNumOfPage = (int) Math.ceil(appliedNumOfRow.getIntValue() / 10.0D);
+		int appliedFreeFirstPage = curAppliedFreePageNum.intValue() / 10 * 10 + 1;
+		int appliedFreeLastPage = Math.min(appliedFreeFirstPage + 9, appliedFreeNumOfPage);
 		
 		
+		// 요청에 모든 변수들을 넘겨줌
 		request.setAttribute("recommListSortOption1st",
-				ProjUtil.getInstance().recommSortOptionToCode(recommSortOption[0]));
+				// 추천프리랜서 목록 정렬옵션 SQL 구문을 다시 코드로 변환함
+				Integer.valueOf(ProjUtil.getInstance().recommSortOptionToCode(recommSortOption[0])));
 		request.setAttribute("recommListSortOption2st",
-				ProjUtil.getInstance().recommSortOptionToCode(recommSortOption[1]));
+				Integer.valueOf(ProjUtil.getInstance().recommSortOptionToCode(recommSortOption[1])));
 		request.setAttribute("recommListSortOption3st",
-				ProjUtil.getInstance().recommSortOptionToCode(recommSortOption[2]));
+				Integer.valueOf(ProjUtil.getInstance().recommSortOptionToCode(recommSortOption[2])));
 		request.setAttribute("appliedFreeListSortOption",
 				ProjUtil.getInstance().appliedSortOptionToCode(appliedFreeListSortOption));
-		System.out.println("쏠트옵션 나가는거 !!!!!!!!!!!!!!???????????????????? " + appliedFreeListSortOption);
 		request.setAttribute("freelancerListToPutInSortOption", freelancerListToPutInSortOption);
-		
+
 		request.setAttribute("projRecruitStateInfo", projRecruitStateInfo);
 		request.setAttribute("curProject", curProject);
 		request.setAttribute("curProjNum", curProjNum);
 		request.setAttribute("recommFreelancer", recommFreelancer);
 		request.setAttribute("appliedFreelancer", appliedFreelancer);
 		request.setAttribute("freelancerListToPutIn", freelancerListToPutIn);
-		
+
 		request.setAttribute("curRecommPageNum", curRecommPageNum);
 		request.setAttribute("curAppliedFreePageNum", curAppliedFreePageNum);
-		request.setAttribute("recommFreeNumOfPage", recommFreeNumOfPage);
-		request.setAttribute("recommFreeFirstPage", recommFreeFirstPage);
-		request.setAttribute("recommFreeLastPage", recommFreeLastPage);
-		request.setAttribute("appliedFreeNumOfPage", appliedFreeNumOfPage);
-		request.setAttribute("appliedFreeFirstPage", appliedFreeFirstPage);
-		request.setAttribute("appliedFreeLastPage", appliedFreeLastPage);
-		
+		request.setAttribute("recommFreeNumOfPage", Integer.valueOf(recommFreeNumOfPage));
+		request.setAttribute("recommFreeFirstPage", Integer.valueOf(recommFreeFirstPage));
+		request.setAttribute("recommFreeLastPage", Integer.valueOf(recommFreeLastPage));
+		request.setAttribute("appliedFreeNumOfPage", Integer.valueOf(appliedFreeNumOfPage));
+		request.setAttribute("appliedFreeFirstPage", Integer.valueOf(appliedFreeFirstPage));
+		request.setAttribute("appliedFreeLastPage", Integer.valueOf(appliedFreeLastPage));
+
 		request.getRequestDispatcher("/project/putfreelancer.jsp").forward(request, response);
 	}
 }
