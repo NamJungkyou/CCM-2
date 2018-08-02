f846661a9d69c8921560d20f1868b4f0255007ea
USER <USER@1.245.186.238>
2018-08-02 16:08:11 +0900

    FreelancerProfileAction 업로드

--- Eureca/src/ccm/controller/action/freeact/FreelancerProfileAction.java
+++ Eureca/src/ccm/controller/action/freeact/FreelancerProfileAction.java
@@ -16,7 +16,12 @@ import ccm.data.table.Education;
 import ccm.data.table.Freelancer;
 import ccm.data.table.ProgLang;
 import ccm.data.table.SkillInventory;
-
+/**
+ * 로그인한 프리랜서의 마이페이지를 불러오는 액션
+ * 
+ * @작성자 글로벌IT꼉영 김민현
+ *
+ */
 public class FreelancerProfileAction implements Action {
 
 	@Override
@@ -26,31 +31,32 @@ public class FreelancerProfileAction implements Action {
 		String url = "/freelancer/freelancerProfile.jsp";
 
 		// String freeid = request.getParameter("freeid");
-
+		
+		// 로그인한 프리랜서의 정보를 불러오기위해 로그인 정보값이 저장된 세션을 불러옴
 		Freelancer loginfree = (Freelancer) request.getSession().getAttribute("loginfree");
 
-		FreelancerDAO fDao = FreelancerDAO.getInstance();
+		FreelancerDAO fDao = FreelancerDAO.getInstance(); // FreelancerDao를 인스턴스값으로 불러옴
 		CommonDAO cDao = CommonDAO.getInstance();
-		Freelancer fVo = fDao.showProfile(loginfree.getFreeId());
-		Freelancer fVo1 = fDao.showAccount(loginfree.getFreeId());
-		System.out.println(fVo.toString());
-		fVo.setFreeBirth(fVo.getFreeBirth().substring(0, 10));
+		Freelancer fVo = fDao.showProfile(loginfree.getFreeId()); // 이름, 나이 등 기본적인 프리랜서 정보 출력
+		Freelancer fVo1 = fDao.showAccount(loginfree.getFreeId()); // 계좌정보 출력
+		System.out.println("프리랜서 기본 정보를 정확히 불러오고 있는지 테스트" + fVo.toString());
+		fVo.setFreeBirth(fVo.getFreeBirth().substring(0, 10)); // 생년월일 중 년월일을 제외한 시간정보는 생략
 		/* Education eVo = fDao.showEducation(loginfree.getFreeId()); */
-		ArrayList<Education> eVo = fDao.showEducation(loginfree.getFreeId());
+		ArrayList<Education> eVo = fDao.showEducation(loginfree.getFreeId()); // 학력정보 출력
 
-		ArrayList<Career> cVo = fDao.showCareer(loginfree.getFreeId());
+		ArrayList<Career> cVo = fDao.showCareer(loginfree.getFreeId()); // 경력정보 출력
 
-		ArrayList<SkillInventory> proVo = fDao.showSkillInventoryPro(loginfree.getFreeId());
+		ArrayList<SkillInventory> proVo = fDao.showSkillInventoryPro(loginfree.getFreeId()); // 스킬인벤토리 출력
 
-		System.out.println(proVo.size() >= 0 ? "안비었음" : "비었음");
+		System.out.println(proVo.size() >= 0 ? "안비었음" : "비었음"); // 스킬인벤토리가 비었는지 안비었는지 테스트
 
-		request.setAttribute("freelancer", fVo);
-		request.setAttribute("freelancer2", fVo1);
+		request.setAttribute("freelancer", fVo); // 프리랜서 기본정보
+		request.setAttribute("freelancer2", fVo1); // 계좌정보
 
-		request.setAttribute("Education", eVo);
-		request.setAttribute("Career", cVo);
+		request.setAttribute("Education", eVo); // 학력정보
+		request.setAttribute("Career", cVo); // 경력정보
 
-		request.setAttribute("SkillInventory", proVo);
+		request.setAttribute("SkillInventory", proVo); // 스킬인벤토리
 		System.out.println("사이즈" + proVo.size());
 		request.setAttribute("langDbFrame", cDao.getLangDBFrame());
 		for(ProgLang temp: cDao.getLangDBFrame().getLang()) {
@@ -62,6 +68,7 @@ public class FreelancerProfileAction implements Action {
 				System.out.println("스킬인벤토리 언어 리스트 = " +lang.getLangName());
 		}
 		
+		// String으로 선언된 url 페이지로 이동
 		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
 		dispatcher.forward(request, response);
 
