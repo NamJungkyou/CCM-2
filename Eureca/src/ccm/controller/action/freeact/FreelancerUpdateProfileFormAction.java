7eb391e8bf446f92c92caf40c0f98e0b3f4bd3f0
USER <USER@1.245.186.238>
2018-08-02 16:10:35 +0900

    FreelancerUpdateProfileFormAction 기존에는 여러파일 중복으로 한번에 올렸으나 혹시 모를 오류때문에 하나씩 업로드

--- Eureca/src/ccm/controller/action/freeact/FreelancerUpdateProfileFormAction.java
+++ Eureca/src/ccm/controller/action/freeact/FreelancerUpdateProfileFormAction.java
@@ -11,24 +11,31 @@ import ccm.controller.action.Action;
 import ccm.dao.FreelancerDAO;
 import ccm.data.table.Freelancer;
 
+/**
+ * 로그인한 프리랜서 기본정보 수정폼을 호출하는 액션
+ * 
+ * @작성자 글로벌IT경영 김민현
+ *
+ */
 public class FreelancerUpdateProfileFormAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String url = "/freelancer/freelancerProfile.jsp";
-		
-		Freelancer loginfree = 
-				(Freelancer)request.getSession().getAttribute("loginfree");
-		
-		FreelancerDAO fDao = FreelancerDAO.getInstance();
-		
-		fDao.updateProfile(loginfree);
-		
-		loginfree = fDao.showProfile(loginfree.getFreeId());
-		
-		request.getSession().setAttribute("loginfree", loginfree);
-		
+
+		// 로그인한 프리랜서의 정보를 불러오기위해 로그인 정보값이 저장된 세션을 불러옴
+		Freelancer loginfree = (Freelancer) request.getSession().getAttribute("loginfree");
+
+		FreelancerDAO fDao = FreelancerDAO.getInstance(); // FreelancerDao를 인스턴스값으로 불러옴
+
+		fDao.updateProfile(loginfree); // 로그인한 프리랜서의 기본정보를 수정하는 메소드
+
+		loginfree = fDao.showProfile(loginfree.getFreeId()); // 로그인한 프리랜서의 기본정보를 출력
+
+		request.getSession().setAttribute("loginfree", loginfree); // 로그인한 프리랜서의 세션값을 불러옴
+
+		// String으로 저장된 url 페이지로 이동
 		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
 		dispatcher.forward(request, response);
 	}
