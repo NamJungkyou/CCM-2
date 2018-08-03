2de5ebe9c586969d3588789250ead43e18e0f84e
jaehwanspin <jaehwanspin@gmail.com>
2018-08-03 17:57:30 +0900

    인터뷰 주석

--- Eureca/src/ccm/data/table/Interview.java
+++ Eureca/src/ccm/data/table/Interview.java
@@ -4,25 +4,34 @@ import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * DB에 생성된 Interview 테이블의 프로퍼티를 모두 담는 클래스
+ * 
+ * @author 글로벌 IT 경영 진재환
+ *
+ */
+
 public class Interview
 {
-	/*
-	
-	INUM BIGINT NOT NULL AUTO_INCREMENT, #면접번호
-  INTVDATE DATE NOT NULL, #면접일
-  LOCATION VARCHAR(15) NOT NULL, #면접장소
-  REASON VARCHAR(100) NULL, #불채용사유
-  STATE INT NOT NULL, #상태
-  FID VARCHAR(15) NOT NULL, #프리랜서아이디
-	
-	*/
+	// 면접번호
 	private Integer intvNum;
+	
+	// 면접일
 	private Date intvDate;
+	
+	// 면접 장소
 	private String intvLocation;
+	
+	// 불채용사유
 	private String intvReason;
+	
+	// 면접상태
 	private Integer intvState;
+	
+	// 프리랜서 아이디
 	private String freeNum;
 	
+	/***********************  게터 세터  *************************/
 	public Integer getIntvNum() {
 		return intvNum;
 	}
@@ -65,6 +74,14 @@ public class Interview
 		return "Interview [intvNum=" + intvNum + ", intvDate=" + intvDate + ", intvLocation=" + intvLocation
 				+ ", intvReason=" + intvReason + ", intvState=" + intvState + ", freeNum=" + freeNum + "]";
 	}
+	
+	/**
+	 * DAO에서 쿼리 실행 후 ResultSet 객체로 받는
+	 * 변수들을 이 클래스 객체의 변수들에 세팅하는 메소드
+	 * 
+	 * @param paramResultSet
+	 * @throws SQLException
+	 */
 	public void setParams(ResultSet rs) throws SQLException
 	{
 		this.intvNum = rs.getInt("interviewNum");
