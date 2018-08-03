2bd3851ce2816ad4f816310eb5f4a523e0b1d360
USER <USER@PC160606KH>
2018-08-03 20:56:03 +0900

    JoinAction.java 업로드

--- Eureca/src/ccm/controller/action/comact/JoinAction.java
+++ Eureca/src/ccm/controller/action/comact/JoinAction.java
@@ -9,22 +9,27 @@ import javax.servlet.http.HttpServletResponse;
 import ccm.controller.action.Action;
 import ccm.dao.CommonDAO;
 import ccm.data.table.Freelancer;
-/*import test.ccm.dao.TestDAO;
-import test.ccm.dto.FreeVO;*/
-
+/**
+ * 회원가입 페이지에서 입력된 정보가 등록되는 회원가입 액션
+ * 커맨드값 : join
+ * 
+ * @작성자 글로벌IT경영 김민현
+ *
+ */
 public class JoinAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		String freeId = request.getParameter("freeId");
-		String freePw = request.getParameter("freePw");
-		String email_front = request.getParameter("email_front");
-		String email_rear = request.getParameter("email_rear");
+		String freeId = request.getParameter("freeId"); // 아이디
+		String freePw = request.getParameter("freePw"); // 비밀번호
+		String email_front = request.getParameter("email_front"); // 이메일 아이디
+		String email_rear = request.getParameter("email_rear"); // 이메일 도메인주소
 
 		System.out.println(freeId + ", " + freePw);
-
-		String freeEmail = email_front + "@" + email_rear;
+		
+		// 이메일 입력값을 받을 때는 아이디와 도메인주소를 따로 입력받지만, 데이터베이스에 넘겨줄때는 아이디와 도메인주소를 한번에 받아서 넘겨줌
+		String freeEmail = email_front + "@" + email_rear; 
 
 		Freelancer fVo = new Freelancer();
 
@@ -33,6 +38,7 @@ public class JoinAction implements Action {
 		fVo.setFreePw(freePw);
 
 		CommonDAO cDao = CommonDAO.getInstance();
+		// CommonDAO에서 회원가입 insertMember 메소드 실행
 		cDao.insertMember(fVo);
 
 	}
