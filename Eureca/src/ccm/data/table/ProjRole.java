48f8b145e444b688ec255d46dead1e4c71d59752
jaehwanspin <jaehwanspin@gmail.com>
2018-08-03 18:32:16 +0900

    주석 최신화

--- Eureca/src/ccm/data/table/ProjRole.java
+++ Eureca/src/ccm/data/table/ProjRole.java
@@ -3,23 +3,42 @@ package ccm.data.table;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class ProjRole {
-	/*
-	 * 
-	 * roleNum BIGINT NOT NULL AUTO_INCREMENT, #역할 번호 roleName VARCHAR(12) NOT NULL,
-	 * #역할 이름 requireNum INT NOT NULL, #필요 인원 projNum BIGINT NULL, #프로젝트 번호
-	 * 
-	 */
+/**
+ * DB에 생성된 ProjRole 테이블의 프로퍼티를 모두 담는 클래스
+ * 
+ * @author 글로벌 IT 경영 진재환
+ *
+ */
 
+public class ProjRole {
+	
+	// 역할 번호
 	private int roleNum;
+	
+	// 역할 이름
 	private String roleName;
+	
+	// 필요인원
 	private int requireNum;
+	
+	// 프로젝트 번호
 	private int projNum;
 
+	/**
+	 * 기본 생성자
+	 */
 	public ProjRole() {
 		super();
 	}
 
+	/**
+	 * 매개변수가 있는 생성자
+	 * 
+	 * @param roleNum
+	 * @param roleName
+	 * @param requireNum
+	 * @param projNum
+	 */
 	public ProjRole(int roleNum, String roleName, int requireNum, int projNum) {
 		super();
 		this.roleNum = roleNum;
@@ -28,6 +47,7 @@ public class ProjRole {
 		this.projNum = projNum;
 	}
 
+	/*********************  게터 세터  **********************/
 	public int getRoleNum() {
 		return roleNum;
 	}
@@ -60,6 +80,13 @@ public class ProjRole {
 		this.projNum = projNum;
 	}
 
+	/**
+	 * DAO에서 쿼리 실행 후 ResultSet 객체로 받는
+	 * 변수들을 이 클래스 객체의 변수들에 세팅하는 메소드
+	 * 
+	 * @param paramResultSet
+	 * @throws SQLException
+	 */
 	public void setParams(ResultSet rs) throws SQLException
 	{
 		this.roleNum = rs.getInt("ROLENUM");
