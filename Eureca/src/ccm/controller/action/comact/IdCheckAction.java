5a11eb2bd8fa2569fb9abeeb4d3c950a4a249530
USER <USER@PC160606KH>
2018-08-03 20:59:54 +0900

    IdCheckAction.java 업로드

--- Eureca/src/ccm/controller/action/comact/IdCheckAction.java
+++ Eureca/src/ccm/controller/action/comact/IdCheckAction.java
@@ -9,25 +9,28 @@ import javax.servlet.http.HttpServletResponse;
 
 import ccm.controller.action.Action;
 import ccm.dao.CommonDAO;
-
+/**
+ * 아이디중복체크가 이루어지는 액션
+ * 
+ * @작성자 글로벌IT경영 김민현
+ *
+ */
 public class IdCheckAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String freeid = request.getParameter("freeid");
-		/*String empid = request.getParameter("freeid");*/
-		/*System.out.println(freeid);*/
-/*		System.out.println("freeid-----" + freeid);
-		System.out.println("empid------" + empid);*/
+		/*System.out.println("freeId값을 제대로 받아오는지 테스트" + freeid);*/
 		CommonDAO cDao = CommonDAO.getInstance();
 		
+		// CommonDAO에서 아이디 중복체크 confirmID 메소드 실행
 		int result = cDao.confirmID(freeid);
 		
 		request.setAttribute("freeid", freeid);
-		/*request.setAttribute("empid", empid);*/
-		request.setAttribute("result", result);
+		request.setAttribute("result", result); // 중복체크가 끝난 freeid값을 result에 입력
 		
+		// 아이디 중복체크가 이루어지는 jsp페이지 출력
 		RequestDispatcher dispatcher = request.getRequestDispatcher("common/idCheck.jsp");
 		dispatcher.forward(request, response);
 
